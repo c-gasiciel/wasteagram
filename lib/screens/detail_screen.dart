b682f1cc8507464a21fdcb17950307065e469587
clarissa <clarissa.gasiciel@gmail.com>
2020-03-10 14:28:50 -0500

    Added location to app

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,19 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   @override
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  LocationData locationData; 
+
+  @override
+  void initState(){
+    super.initState();
+    retrieveLocation();
+  }
+
+  void retrieveLocation() async{
+    var locationService = Location();
+    locationData = await locationService.getLocation();
+    setState( () {});
+  }
+
+
+
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Wasteagram'))
+      ),
+      body: displayLoc(locationData)
+    );
+  }
+}
+
+
+Widget displayLoc(LocationData locData){
+  if(locData == null){
+    return Center(child: CircularProgressIndicator());
+  } else{
     return Center(
-      child: Row(children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text('( ,'),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(' )'),
-        )
-      ])  
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Row(
+            children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('(${locData.latitude},'),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(' ${locData.longitude})'),
+              ),
+            )
+          ]),
+        ]),
     );
   }
 }
